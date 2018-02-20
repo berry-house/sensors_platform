ff388ecb90e883ce453b66df9c85922a5f75a306
carlosmartell97 <carlosmartell97@gmail.com>
2018-02-20 15:42:24 -0600

    refactored code into a single script

---
+++ humiture_and_light.py
@@ -0,0 +1,153 @@
+import RPi.GPIO as GPIO
+import PCF8591 as ADC
+import time
+import os
+import requests
+#url = "http://localhost:8000/broker/temperature"
+url = "http://35.230.39.10:80/broker/temperature"
+
+DHTPIN = 17
+DO = 17
+GPIO.setmode(GPIO.BCM)
+
+MAX_UNCHANGE_COUNT = 100
+
+STATE_INIT_PULL_DOWN = 1
+STATE_INIT_PULL_UP = 2
+STATE_DATA_FIRST_PULL_DOWN = 3
+STATE_DATA_PULL_UP = 4
+STATE_DATA_PULL_DOWN = 5
+
+
+def setup():
+	ADC.setup(0x48)
+	GPIO.setup(DO, GPIO.IN)
+
+
+def read_dht11_dat():
+	GPIO.setup(DHTPIN, GPIO.OUT)
+	GPIO.output(DHTPIN, GPIO.HIGH)
+	time.sleep(0.05)
+	GPIO.output(DHTPIN, GPIO.LOW)
+	time.sleep(0.02)
+	GPIO.setup(DHTPIN, GPIO.IN, GPIO.PUD_UP)
+
+	unchanged_count = 0
+	last = -1
+	data = []
+	while True:
+		current = GPIO.input(DHTPIN)
+		data.append(current)
+		if last != current:
+			unchanged_count = 0
+			last = current
+		else:
+			unchanged_count += 1
+			if unchanged_count > MAX_UNCHANGE_COUNT:
+				break
+
+	state = STATE_INIT_PULL_DOWN
+
+	lengths = []
+	current_length = 0
+
+	for current in data:
+		current_length += 1
+
+		if state == STATE_INIT_PULL_DOWN:
+			if current == GPIO.LOW:
+				state = STATE_INIT_PULL_UP
+			else:
+				continue
+		if state == STATE_INIT_PULL_UP:
+			if current == GPIO.HIGH:
+				state = STATE_DATA_FIRST_PULL_DOWN
+			else:
+				continue
+		if state == STATE_DATA_FIRST_PULL_DOWN:
+			if current == GPIO.LOW:
+				state = STATE_DATA_PULL_UP
+			else:
+				continue
+		if state == STATE_DATA_PULL_UP:
+			if current == GPIO.HIGH:
+				current_length = 0
+				state = STATE_DATA_PULL_DOWN
+			else:
+				continue
+		if state == STATE_DATA_PULL_DOWN:
+			if current == GPIO.LOW:
+				lengths.append(current_length)
+				state = STATE_DATA_PULL_UP
+			else:
+				continue
+	if len(lengths) != 40:
+		print "Data not good, skip"
+		return False
+
+	shortest_pull_up = min(lengths)
+	longest_pull_up = max(lengths)
+	halfway = (longest_pull_up + shortest_pull_up) / 2
+	bits = []
+	the_bytes = []
+	byte = 0
+
+	for length in lengths:
+		bit = 0
+		if length > halfway:
+			bit = 1
+		bits.append(bit)
+	#print "bits: %s, length: %d" % (bits, len(bits))
+	for i in range(0, len(bits)):
+		byte = byte << 1
+		if (bits[i]):
+			byte = byte | 1
+		else:
+			byte = byte | 0
+		if ((i + 1) % 8 == 0):
+			the_bytes.append(byte)
+			byte = 0
+	#print the_bytes
+	checksum = (the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3]) & 0xFF
+	if the_bytes[4] != checksum:
+		print "Data not good, skip"
+		return False
+
+	return the_bytes[0], the_bytes[2]
+
+
+def main():
+	print "Berry house sensors platform!\n"
+	status = 1
+        while True:
+                #print 'Light: ', ADC.read(0)
+                #time.sleep(0.2)
+		result = read_dht11_dat()
+		if result:
+			humidity, temperature = result
+			print "Light:%s  Humidity: %s %%,  Temperature: %s C`" % (ADC.read(0), humidity, temperature)
+			data = {
+				"id": 1,
+				"timestamp": 1516740522,
+				"temperature": temperature,
+			}
+			resp = requests.post(url, json=data)
+			print(" URL:\t\t%s" % resp.url)
+			print(" encoding:\t%s" % resp.encoding)
+			print(" status_code:\t%s" % resp.status_code)
+			print(" text:\t\t%s" % resp.text)
+		time.sleep(1)
+
+
+def destroy():
+	GPIO.cleanup()
+
+
+if __name__ == '__main__':
+	try:
+		setup()
+		main()
+		loop()
+	except KeyboardInterrupt:
+		pass
+		destroy()
